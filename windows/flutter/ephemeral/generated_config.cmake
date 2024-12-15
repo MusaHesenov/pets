903a746c7c294a333818aea54b26dc2f336f5f8b
MusaHesenov <musa.hesenov.2018@gmail.com>
2024-12-15 21:16:15 +0400

    firebase updt1

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter source\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Agsaggal\\FlutterProjects\\petsdemo" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter source\\flutter"
+  "PROJECT_DIR=C:\\Users\\Agsaggal\\FlutterProjects\\petsdemo"
+  "FLUTTER_ROOT=C:\\flutter source\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Agsaggal\\FlutterProjects\\petsdemo\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Agsaggal\\FlutterProjects\\petsdemo"
+  "FLUTTER_TARGET=C:\\Users\\Agsaggal\\FlutterProjects\\petsdemo\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Agsaggal\\FlutterProjects\\petsdemo\\.dart_tool\\package_config.json"
+)
